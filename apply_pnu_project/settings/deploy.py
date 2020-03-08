c825601fabeb3d14207d98efc2c9880e0e674ba1
Tedhoon <gt0305@likelion.org>
2020-03-08 20:37:35 +0900

    for dp

--- apply_pnu_project/settings/deploy.py
+++ apply_pnu_project/settings/deploy.py
@@ -5,16 +5,26 @@ import json
 
 DEBUG = False 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pnu.likelion.org','.amazonaws.com','localhost']
 
 WSGI_APPLICATION = 'apply_pnu_project.wsgi'
 
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DB_PW = config_secret_deploy['django']['db']['password']
+DB_NAME = config_secret_deploy['django']['db']['name']
+DB_USER = config_secret_deploy['django']['db']['user']
+DB_HOST = config_secret_deploy['django']['db']['host']
+
+
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
