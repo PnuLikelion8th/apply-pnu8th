4e4fcaa437095c048c591400c32e06aaf834fc52
chichi <jang.chihun@gmail.com>
2020-03-11 18:13:37 +0900

    [수정] 지원서 제출 개행문자 에러 수정 및 detail 페이지 개행 추가

--- apply_pnu/static/js/counting_text.js
+++ apply_pnu/static/js/counting_text.js
@@ -1,9 +1,15 @@
 const temp_form = document.querySelectorAll('.custom_textarea')
 Array.from(temp_form).forEach(item => {
     item.addEventListener('keyup', (e)=>{
-        // console.log(e.target.value)
-        // console.log(e.target.value.length)
-        // console.log(e.target.nextSibling.nextSibling.innerText)
-        e.target.nextSibling.nextSibling.innerText = '글자수 : ' +  e.target.value.length
+
+        let check_enter = 0
+        for (let i = 0; i < e.target.value.length; i++){
+            if (e.target.value[i] === "\n"){
+                check_enter++
+            }
+        }
+    
+        
+        e.target.nextSibling.nextSibling.innerText = '글자수 : ' + (e.target.value.length +check_enter)
     }, false)
 });
